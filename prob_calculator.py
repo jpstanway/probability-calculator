02f374c4533810fd2819561cca1a331e67b4666f
Jordan S <jpstanway@gmail.com>
2021-08-25 21:36:55 -0700

    create draw method to get random nums from contents

--- prob_calculator.py
+++ prob_calculator.py
@@ -6,13 +6,25 @@ import random
 class Hat:
     def __init__(self, **kwargs):
         self.balls = kwargs
-        self.content = []
+        self.contents = []
 
         for ball in self.balls:
             counter = self.balls[ball]
             while (counter > 0):
-                self.content.append(ball)
+                self.contents.append(ball)
                 counter -= 1
+
+    def draw(self, num):
+        count = num
+        selections = []
+
+        while (count > 0):
+            idx = random.randint(0, len(self.contents) - 1)
+            ball = self.contents.pop(idx)
+            selections.append(ball)
+            count -= 1
+
+        return selections
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
