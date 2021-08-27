0755e70d86ddf3117adc7e05e19a982f7cb56590
Jordan S <jpstanway@gmail.com>
2021-08-26 18:48:34 -0700

    create experiment method

--- prob_calculator.py
+++ prob_calculator.py
@@ -18,14 +18,34 @@ class Hat:
         count = num
         selections = []
 
-        while (count > 0):
-            idx = random.randint(0, len(self.contents) - 1)
-            ball = self.contents.pop(idx)
-            selections.append(ball)
-            count -= 1
+        if (num <= len(self.contents)):
+            while (count > 0):
+                idx = random.randint(0, len(self.contents) - 1)
+                ball = self.contents.pop(idx)
+                selections.append(ball)
+                count -= 1
 
         return selections
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    return True
+    instance = hat
+    matches = 0
+    counter = num_experiments
+
+    while (counter > 0):
+        draw = instance.draw(num_balls_drawn)
+        isEqual = False
+
+        for ball in expected_balls:
+            if(draw.count(ball) == expected_balls[ball]):
+                isEqual = True
+            else:
+                isEqual = False
+
+        if (isEqual):
+            matches += 1
+
+        counter -= 1
+
+    return matches / num_experiments
